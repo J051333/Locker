d451048e9e3312d74abe9ebd1c8acc453dc1f5ad
Josi Scarlett Whitlock <62673737+J051333@users.noreply.github.com>
2023-07-07 02:37:31 -0500

    Added Monitor Indicators

---
+++ Locker/MonitorIndicator.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Locker {
+    partial class MonitorIndicator {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.monitorNum = new System.Windows.Forms.Label();
+            this.Esc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monitorNum
+            // 
+            this.monitorNum.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monitorNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F);
+            this.monitorNum.Location = new System.Drawing.Point(0, 0);
+            this.monitorNum.Name = "monitorNum";
+            this.monitorNum.Size = new System.Drawing.Size(422, 295);
+            this.monitorNum.TabIndex = 0;
+            this.monitorNum.Text = "Mon";
+            this.monitorNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Esc
+            // 
+            this.Esc.AutoSize = true;
+            this.Esc.Location = new System.Drawing.Point(12, 9);
+            this.Esc.Name = "Esc";
+            this.Esc.Size = new System.Drawing.Size(66, 13);
+            this.Esc.TabIndex = 1;
+            this.Esc.Text = "Esc to Close";
+            // 
+            // MonitorIndicator
+            // 
+            this.ClientSize = new System.Drawing.Size(422, 295);
+            this.Controls.Add(this.Esc);
+            this.Controls.Add(this.monitorNum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MonitorIndicator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label monitorNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label monitorNum;
+        private System.Windows.Forms.Label Esc;
+    }
+}
---
+++ Locker/MonitorIndicator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Locker {
+    public partial class MonitorIndicator : Form {
+        public static readonly Size size = new Size(400, 400);
+        public delegate void DisposeOfMIs();
+        public DisposeOfMIs Dis;
+
+        public MonitorIndicator() {
+            InitializeComponent();
+        }
+
+        public void SetIndicator(int i) {
+            this.monitorNum.Text = (i + 1).ToString();
+            this.Size = size;
+            this.CenterToScreen();
+
+            int x = this.Location.X, y = this.Location.Y;
+
+            x += Screen.AllScreens[i].WorkingArea.Location.X;
+            y += Screen.AllScreens[i].WorkingArea.Location.Y;
+
+            this.Location = new Point(x, y);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
+            if (keyData == Keys.Escape) {
+                Dis();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
